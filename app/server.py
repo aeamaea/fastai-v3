af510fa64bb554a4d80734417ada5e1c493cb98f
aeamaea <mike.moloch@gmail.com>
2019-05-16 15:30:51 -0400

    added the pickle file and classes to the server.py file

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+#export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url='https://drive.google.com/open?id=1opR2ZJ7KAS61qWOd2DjXr8GCEjHZFCx9'
 
-classes = ['black', 'grizzly', 'teddys']
+export_file_name = 'resnet34_22epoch_065347error_1.pkl'
+
+classes = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
 path = Path(__file__).parent
 
 app = Starlette()
